7526539ea6905dc81cc625a3c3612105e0efd4a8
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2019-07-07 22:57:44 -0400

    Add tool to diff worlds.

---
+++ src/diff_db.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+
+import os, sys, argparse, json
+
+class DatabaseFile(object):
+    def __init__(self, path, filename):
+        self._path = path
+        self._filename = filename
+        self.load()
+
+    def __str__(self):
+        return "\n".join(map(str, self.entities))
+
+    def load(self):
+        full_path = os.path.join(self._path, "data", self._filename)
+        self.entities = {}
+        with open(full_path, "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                new_entity = json.loads(line)
+                self.entities[new_entity["_id"]] = new_entity
+
+
+class Differ(object):
+    def __init__(self, args):
+        self.args = args
+
+        worlds = [self.load_world(args.left), self.load_world(args.right)]
+        self.diff_json("", worlds[0], worlds[1])
+
+    def load_world(self, path):
+        with open(os.path.join(path, "world.json"), "r") as f:
+            world = {"world": json.load(f)}
+        for db in ["users", "sessions", "folders", "items", "scenes", "journal", "actors", "playlists", "combat", "settings", "chat"]:
+            world[db] = DatabaseFile(path, db + ".db").entities
+
+        return world
+
+    def diff_json(self, path, left, right):
+        if type(left) != type(right):
+            print "%s: type mismatch between left and right elements : %s != %s" % (path, type(left), type(right))
+        elif type(left) == dict:
+            for key in left:
+                if key in right:
+                    self.diff_json(os.path.join(path, key), left[key], right[key])
+                else:
+                    print "%s: Key %s only in left json" % (path, key)
+            for key in right:
+                if key not in left:
+                    print "%s: Key %s only in right json" % (path, key)
+        elif type(left) == list:
+            if len(left) != len(right):
+                print "%s: Left and right lists have different length : %s != %s" % (path, len(left), len(right))
+            else:
+                for i in xrange(len(left)):
+                    self.diff_json(os.path.join(path, str(i)), left[i], right[i])
+        elif type(left) == str:
+            if left != right:
+                print "%s: Strings are different" % path
+        else:
+            if left != right:
+                try:
+                    print "%s: Values are different : '%s'(%s) != '%s'(%s)" % (path, str(left), type(left), str(right), type(right))
+                except:
+                    print "%s: non-printable values are different : ''(%s) != ''(%s)" % (path, type(left), type(right))
+
+                    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="FVTT Database Differ", epilog="Compare databases from two worlds.")
+    parser.add_argument("left", metavar="first-world", help="The first world's directory in public/worlds/")
+    parser.add_argument("right", metavar="second-world", help="The secnd world's directory in public/worlds/")
+    args = parser.parse_args()
+
+    diff = Differ(args)
+        
