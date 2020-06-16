40d051d8be658f609fdc7e4f8982e57505e20c91
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2020-06-15 21:44:03 -0400

    Switch to cx_freeze instead of py2exe

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
-from distutils.core import setup
-import py2exe
+import sys
+from cx_Freeze import setup, Executable
 
-setup(
-    options = {
-        "py2exe":{
-            "dll_excludes": ["MSVCP90.dll", "HID.DLL", "w9xpopen.exe"],
-            "dist_dir": "windows"
-            }
-    },
-    console=['src/R20Converter.py'])
+sys.path.append("src")
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+buildOptions = dict(packages = [], excludes = ["tkinter"])
+
+base = 'Console'
+
+executables = [
+    Executable('src/R20Converter.py', base=base, targetName = 'R20Converter')
+]
+
+setup(name='R20Converter',
+      version = '0.8.98',
+      description = 'Convert a Roll 20 Campaign into a Foundry VTT world',
+      options = dict(build_exe = buildOptions),
+      executables = executables,
+      data_files = [ "templates"]
+      )
